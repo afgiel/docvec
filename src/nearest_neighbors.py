d0ff8fcd46d4ae353dc95327285df66cd7269bbb
ryandiaz <diazry@gmail.com>
2015-06-02 17:36:48 -0700

    nearest neighbors added

---
+++ src/nearest_neighbors.py
@@ -0,0 +1,14 @@
+from sklearn.neighbors import NearestNeighbors
+import numpy as np
+
+
+class DocNN():
+
+    def __init__(self, D, k=1):
+        self.D = D
+        self.nbrs = NearestNeighbors(n_neighbors=k+1, algorithm='auto').fit(D)
+
+    def doc_nearest(doc_ind):
+        distances, indices = nbrs.kneighbors(self.D)
+        neighbors = indices[doc_ind][1:]
+        return self.D[neighbors]
